43791bbd62e989f309db9de8168d313adeb0067d
Angus Stewart <siliconlad@protonmail.com>
2021-03-28 02:56:18 +0100

    Convert to ES modules

--- test/app.test.js
+++ test/app.test.js
@@ -1,58 +1,52 @@
-const request = require("supertest");
-const mongoose = require("mongoose");
-const app = require("../app");
+import request from "supertest";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "../app.js";
 
 // Read .env file for secrets
-require('dotenv').config()
+dotenv.config();
 
-describe('Server-side Tests', function () {
-    // Stores the database connection
-    let db;
+describe("Server-side Tests", function () {
+  // Stores the database connection
+  let db;
 
-    // Connect to the database for all tests
-    before('Connect to database', async function () {
-        await mongoose.connect(process.env.TEST_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-        db = mongoose.connection;
+  // Connect to the database for all tests
+  before("Connect to database", async function () {
+    await mongoose.connect(process.env.TEST_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = mongoose.connection;
+  });
+
+  describe("GET /", function () {
+    it("returns 200 OK", function (done) {
+      request(app).get("/").expect(200, done);
+    });
+  });
+
+  describe("GET /robots", function () {
+    it("returns 200 OK", function (done) {
+      request(app).get("/robots").expect(200, done);
     });
 
-    describe('GET /', function () {
-        it('returns 200 OK', function (done) {
-            request(app)
-                .get('/')
-                .expect(200, done);
-        });
+    it("responds with json", function (done) {
+      request(app).get("/robots").expect("Content-Type", /json/, done);
+    });
+  });
+
+  describe("GET /robot_details", function () {
+    it("returns 200 OK", function (done) {
+      request(app).get("/robot_details").expect(200, done);
     });
 
-    describe('GET /robots', function () {
-        it('returns 200 OK', function (done) {
-            request(app)
-                .get('/robots')
-                .expect(200, done);
-        });
-
-        it('responds with json', function (done) {
-            request(app)
-                .get('/robots')
-                .expect('Content-Type', /json/, done);
-        });
+    it("responds with json", function (done) {
+      request(app).get("/robot_details").expect("Content-Type", /json/, done);
     });
+  });
 
-    describe('GET /robot_details', function () {
-        it('returns 200 OK', function (done) {
-            request(app)
-                .get('/robot_details')
-                .expect(200, done);
-        });
-
-        it('responds with json', function (done) {
-            request(app)
-                .get('/robot_details')
-                .expect('Content-Type', /json/, done);
-        });
-    });
-
-    // Close connection to database
-    after('Close connection to database', function () {
-        db.close();
-    });
+  // Close connection to database
+  after("Close connection to database", function () {
+    db.close();
+  });
 });
